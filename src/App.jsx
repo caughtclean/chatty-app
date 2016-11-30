380fdffb3ac86896a3213577404f596f44299616
Nathan Eyerley <eyerleyn@gmail.com>
2016-11-30 04:07:40 +0000

    added chat input that will dynamically update the messages and set the state

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import MessageList from './messageList.jsx';
 import ChatBar from './ChatBar.jsx';
 class App extends Component {
 
+
+
+
   constructor(props) {
     super(props);
     this.state = {
@@ -19,6 +22,28 @@ class App extends Component {
         }
       ]
     };
+
+    this.test = this.test.bind(this)
+  };
+
+
+      test (chatinput, username) {
+        const newMessage = {id: Math.random(), username: username, content: chatinput};
+        const messages = this.state.messages.concat(newMessage)
+        this.setState({messages: messages})
+        console.log(chatinput)
+      }
+
+
+
+  componentDidMount() {
+    console.log("componentDidMount <App />");
+    setTimeout(() => {
+      console.log("Simulating incoming message");
+      const newMessage = {id: 3, username: "Michelle", content: "Hello there!"};
+      const messages = this.state.messages.concat(newMessage)
+      this.setState({messages: messages})
+    }, 3000);
   }
 
   render() {
@@ -29,7 +54,8 @@ class App extends Component {
           <h1>Chatty</h1>
         </nav>
         <MessageList messages={this.state.messages}/>
-        <ChatBar currentUser={this.state.currentUser.name}/>
+        <ChatBar currentUser={this.state.currentUser.name}
+                 test={this.test}/>
       </div>
     );
   }
--- src/ChatBar.jsx
+++ src/ChatBar.jsx
@@ -1,12 +1,47 @@
 import React, {Component} from 'react';
-
 class ChatBar extends Component {
+ constructor(props) {
+    super(props);
+    this.state = {value: ''};
+
+
+    this.handleChange = this.handleChange.bind(this);
+
+  }
+
+  handleChange(event) {
+    if (event.key === "Enter") {
+      this.props.test(event.target.value, this.props.currentUser);
+    }
+
+    else {
+     this.setState({value: event.target.value});
+     console.log(this.state.value)
+    }
+
+
+
+
+  }
+
+  // handleSubmit(event) {
+  //   if (event.key === "Enter") {
+  //   console.log("text submited" + this.state.value);
+  //   }
+
+  // }
+
   render() {
     console.log("Rendering ChatBar")
     return (
    <footer>
-    <input id="username" type="text" placeholder="Your Name (Optional)" value={this.props.currentUser} />
-    <input id="new-message" type="text" placeholder="Type a message and hit ENTER" />
+    <form>
+      <input id="username" type="text" placeholder="Your Name (Optional)" value={this.props.currentUser}
+      onChange={this.handleChange} />
+      <input id="new-message" type="text"
+        onKeyPress={this.handleChange}
+        placeholder="Type a message and hit ENTER" />
+    </form>
   </footer>
      );
   }
