8eb2f2a0733bc0b843b85aecd0bb764c05fc98c4
Nathan Eyerley <eyerleyn@gmail.com>
2016-11-30 00:46:27 +0000

    added dummy data, started passing props to username field and messageList fields.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,38 @@
 import React, {Component} from 'react';
-
+import MessageList from './messageList.jsx';
+import ChatBar from './ChatBar.jsx';
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentUser: {name: "Bob"},
+      messages: [
+        {
+          username: "Bob",
+          content: "Has anyone seen my marbles?"
+        },
+
+        {
+          username: "Anonymous",
+          content: "No, I think you lost them. You lost your marbles Bob. You lost them for good."
+        }
+      ]
+    };
+  }
+
   render() {
+    console.log("Rendering <App/>");
     return (
-      <h1>Hello React :)</h1>
+      <div>
+        <nav>
+          <h1>Chatty</h1>
+        </nav>
+        <MessageList messages={this.state.messages}/>
+        <ChatBar currentUser={this.state.currentUser.name}/>
+      </div>
     );
   }
 }
+
 export default App;
